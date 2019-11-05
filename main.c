db36106f32ce94968c13256836c445fa7106adb3
drlotalota <57364358+drlotalota@users.noreply.github.com>
2019-11-05 18:57:58 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+// This text right here is single-line comment.
+
+/* This text right here is comment as well but this is multi-line comment.
+Second line of text. */
+
+/*
+I will comment what each line of code does using comments in next commit.
+*/
+
+#include <stdio.h>
+int main(void) {
+    printf("You are fantastic.\n");
+    return 0;
+}
